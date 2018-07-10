fd2ee7f7cac4ad5ac2ecd76eeecdf6c2ee98a782
Zitong Guo <jsguozitong@gmail.com>
2018-07-10 15:58:39 +0800

    Update 189. Rotate Array.py

--- 189. Rotate Array.py
+++ 189. Rotate Array.py
@@ -1,1 +1,48 @@
+# coding=utf-8
+"""
+Question:
+Given an array, rotate the array to the right by k steps, where k is non-negative.
 
+Example 1:
+
+Input: [1,2,3,4,5,6,7] and k = 3
+Output: [5,6,7,1,2,3,4]
+Explanation:
+rotate 1 steps to the right: [7,1,2,3,4,5,6]
+rotate 2 steps to the right: [6,7,1,2,3,4,5]
+rotate 3 steps to the right: [5,6,7,1,2,3,4]
+
+Example 2:
+
+Input: [-1,-100,3,99] and k = 2
+Output: [3,99,-1,-100]
+Explanation:
+rotate 1 steps to the right: [99,-1,-100,3]
+rotate 2 steps to the right: [3,99,-1,-100]
+
+Note:
+
+- Try to come up as many solutions as you can, there are at least 3 different ways to solve this problem.
+- Could you do it in-place with O(1) extra space?
+
+Link: https://leetcode.com/problems/rotate-array/
+"""
+
+class Solution(object):
+    def rotate(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: void Do not return anything, modify nums in-place instead.
+        """
+        while k > 0:
+            nums.insert(0, nums.pop())
+            k -= 1
+
+if __name__ == '__main__':
+    a1, k1 = [1,2,3,4,5,6,7], 3
+    Solution().rotate(a1, k1)
+    print a1
+    a2, k2 = [-1,-100,3,99], 2
+    Solution().rotate(a2, k2)
+    print a2
